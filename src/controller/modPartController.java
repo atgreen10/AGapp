380ef148108901d23a55c647347fac8ea6091e41
atgreen10 <35249183+atgreen10@users.noreply.github.com>
2020-09-20 20:02:27 -0500

    Update modPartController.java

--- src/controller/modPartController.java
+++ src/controller/modPartController.java
@@ -10,8 +10,13 @@ import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+import model.InHouse;
+import model.Inventory;
+import model.Outsource;
+import model.Part;
 
 import java.io.IOException;
+
 
 public class modPartController {
 
@@ -51,8 +56,69 @@ public class modPartController {
     @FXML
     private Button modPartCancelBtn;
 
+
     @FXML
     void onClickModPartCancel(MouseEvent event) throws IOException {
+        stage = (Stage) ((Button) event.getSource()).getScene().getWindow();
+        scene = FXMLLoader.load(getClass().getResource("/view/mainMenu.fxml"));
+        stage.setScene(new Scene(scene));
+        stage.show();
+    }
+
+    public void sendPart(Part part) {
+        modPartID.setText(String.valueOf(part.getPartID()));
+        modPartName.setText(part.getPartName());
+        modPartStock.setText(String.valueOf(part.getPartStock()));
+        modPartPrice.setText(String.valueOf(part.getPartPrice()));
+        modPartMax.setText(String.valueOf(part.getPartMax()));
+        modPartMin.setText(String.valueOf(part.getPartMin()));
+
+        if (part instanceof InHouse) {
+            inHouseBtn.setSelected(true);
+            modPartSourceTxt.setText(String.valueOf(((InHouse) part).getMachineID()));
+
+        } else {
+            outsourcedBtn.setSelected(true);
+            modPartSourceTxt.setText(((Outsource) part).getCompanyName());
+        }
+    }
+
+
+    @FXML
+    void onClickSavePartMods(MouseEvent event) throws IOException{
+
+        int updatedPartID;
+        String updatedPartName;
+        int partStock;
+        double partPrice;
+        int partMax;
+        int partMin;
+
+        if(inHouseBtn.isSelected()){
+            updatedPartID = Integer.parseInt(modPartID.getText());
+            updatedPartName = modPartName.getText();
+            partStock = Integer.parseInt(modPartStock.getText());
+            partPrice = Double.parseDouble(modPartPrice.getText());
+            partMax = Integer.parseInt(modPartMax.getText());
+            partMin = Integer.parseInt(modPartMin.getText());
+            int machineID = Integer.parseInt(modPartSourceTxt.getText());
+
+            Inventory.updatePart(new InHouse(updatedPartID, updatedPartName, partPrice, partStock, partMax, partMin, machineID));
+        }
+
+        else if(outsourcedBtn.isSelected()) {
+
+            updatedPartID = Integer.parseInt(modPartID.getText());
+            updatedPartName = modPartName.getText();
+            partStock = Integer.parseInt(modPartStock.getText());
+            partPrice = Double.parseDouble(modPartPrice.getText());
+            partMax = Integer.parseInt(modPartMax.getText());
+            partMin = Integer.parseInt(modPartMin.getText());
+            String companyName = modPartSourceTxt.getText();
+
+            Inventory.updatePart(new Outsource(updatedPartID, updatedPartName, partPrice, partStock, partMax, partMin, companyName));
+        }
+
         stage = (Stage) ((Button) event.getSource()).getScene().getWindow();
         scene = FXMLLoader.load(getClass().getResource("/view/mainMenu.fxml"));
         stage.setScene(new Scene(scene));
