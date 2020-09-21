99edc55d0953f99687b40bbc11826c0566c36174
atgreen10 <35249183+atgreen10@users.noreply.github.com>
2020-09-20 20:02:07 -0500

    Update mainMenuController.java

--- src/controller/mainMenuController.java
+++ src/controller/mainMenuController.java
@@ -1,24 +1,23 @@
 package controller;
 
+import javafx.beans.binding.ObjectExpression;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 import model.Inventory;
-import model.Outsource;
 import model.Part;
 import model.Product;
 
 import java.io.IOException;
-import java.net.URL;
-import java.util.ResourceBundle;
+import java.util.Optional;
+
 
 public class mainMenuController {
 
@@ -39,19 +38,6 @@ public class mainMenuController {
 
     @FXML
     private TableColumn<Part, Double> mainTablePartPrice;
-
-
-    @FXML
-    private Button mainPartSearchBtn;
-
-    @FXML
-    private Button mainAddPartBtn;
-
-    @FXML
-    private Button mainModPartBtn;
-
-    @FXML
-    private Button mainDeletePartBtn;
 
     @FXML
     private TextField mainPartSearchTxt;
@@ -90,31 +76,76 @@ public class mainMenuController {
     private Button exitAppBtn;
 
     @FXML
+    private ObservableList<Part> partSearchList = FXCollections.observableArrayList();
+
+    @FXML private ObservableList<Product> productSearchList = FXCollections.observableArrayList();
+
+
+    @FXML
     void onClickMainAddPartBtn(MouseEvent event) throws IOException {
-        stage = (Stage) ((Button)event.getSource()).getScene().getWindow();
-        scene = FXMLLoader.load(getClass().getResource("/view/partMenu.fxml"));
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("/view/partMenu.fxml"));
+        loader.load();
+        stage = (Stage) ((Button) event.getSource()).getScene().getWindow();
+        Parent scene = loader.getRoot();
         stage.setScene(new Scene(scene));
         stage.show();
     }
 
     @FXML
     void onClickMainPartMod(MouseEvent event) throws IOException {
-        stage = (Stage) ((Button)event.getSource()).getScene().getWindow();
-        scene = FXMLLoader.load(getClass().getResource("/view/modPartMenu.fxml"));
+
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("/view/modPartMenu.fxml"));
+        loader.load();
+
+        modPartController modPartCtrl = loader.getController();
+        modPartCtrl.sendPart(mainPartTable.getSelectionModel().getSelectedItem());
+
+        stage = (Stage) ((Button) event.getSource()).getScene().getWindow();
+        Parent scene = loader.getRoot();
         stage.setScene(new Scene(scene));
         stage.show();
     }
 
     @FXML
     void onClickMainAddProdBtn(MouseEvent event) throws IOException {
-        stage = (Stage) ((Button)event.getSource()).getScene().getWindow();
+        stage = (Stage) ((Button) event.getSource()).getScene().getWindow();
         scene = FXMLLoader.load(getClass().getResource("/view/prodMenu.fxml"));
         stage.setScene(new Scene(scene));
         stage.show();
     }
 
     @FXML
+    void onClickPartSearch(MouseEvent event) {
+
+       if(!mainPartSearchTxt.getText().isEmpty()){
+           partSearchList.clear();
+           for(Part part : Inventory.getAllParts()){
+               if(part.getPartName().contains(mainPartSearchTxt.getText())){
+                   partSearchList.add(part);
+               }
+               if(mainPartSearchTxt.getText().contains(String.valueOf(part.getPartID()))){
+                   partSearchList.add(part);
+               }
+           }
+           mainPartTable.setItems(partSearchList);
+           mainPartTable.refresh();
+       }
+       else{
+           mainPartTable.refresh();
+       }
+    }
+
+    @FXML
     void onClickMainDeletePartBtn(MouseEvent event) {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION, "Are you sure you want to delete this part?");
+        Optional<ButtonType> result = alert.showAndWait();
+
+        if(result.isPresent() && result.get() == ButtonType.OK){
+            Part selectPart = mainPartTable.getSelectionModel().getSelectedItem();
+            Inventory.deletePart(selectPart);
+        }
 
     }
 
@@ -126,28 +157,52 @@ public class mainMenuController {
     @FXML
     void onClickMainExitBtn(MouseEvent event) {
         System.exit(0);
-
     }
 
     @FXML
     void onClickMainModProdBtn(MouseEvent event) throws IOException {
-        stage = (Stage) ((Button)event.getSource()).getScene().getWindow();
+       stage = (Stage) ((Button) event.getSource()).getScene().getWindow();
         scene = FXMLLoader.load(getClass().getResource("/view/modProdMenu.fxml"));
         stage.setScene(new Scene(scene));
         stage.show();
     }
+
+
     public void initialize() {
-        //sets up table view
+        //sets up table view with regular default view
         mainPartTable.setItems(Inventory.getAllParts());
+
+
         //Describes where the columns get their info.
         mainTablePartID.setCellValueFactory(new PropertyValueFactory<>("partID"));
         mainTablePartName.setCellValueFactory(new PropertyValueFactory<>("partName"));
         mainTablePartPrice.setCellValueFactory(new PropertyValueFactory<>("partPrice"));
         mainTablePartStock.setCellValueFactory(new PropertyValueFactory<>("partStock"));
 
+       /* if(searchPart(33)){
+            System.out.println("match");
+        }
+        else{
+            System.out.println("No match");
+        }
+
+        if (updatePart(2, new InHouse(2, "NewPart", 49.99, 20, 10, 1, 200, true))) {
+            System.out.println("Update Successful");
+        } else{
+            System.out.println("update failed");
+        }
+
+        if(deletePart(3)){
+            System.out.println("Deleted Part");
+        }
+        else{
+            System.out.println("No Match!");
+        }*/
+
+       // mainPartTable.getSelectionModel().select(selectPart(2));
+
 
 
     }
-
 
 }
